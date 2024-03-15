0652352ab98d6e868391953193e36f31f88f6158
Rahulxpro <Rahulkumar073011@gmail.com>
2024-03-15 22:53:33 +0530

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,15 @@ import "../styles/App.css";
 
 const App = () => {
   return (
-    
-
+    <div>
+      {/* Navigation Bar */}
+      {/* Main Content Section */}
       {/* Use Below Images for the making articles */}
       {/* Football - https://shorturl.at/ltBRY */}
       {/* Basketball - https://shorturl.at/ejvNY */}
       {/* Tennis - https://shorturl.at/hLQRS */}
 
       {/* Footer */}
-      <footer id="footer">
-        <p class="footer-content">
-          © 2023 Sports Website. All rights reserved.
-        </p>
-      </footer>
     </div>
   );
 };
